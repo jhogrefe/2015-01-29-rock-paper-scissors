16e7b5d9d6064392979990ba838b3876ed72eca9
Jason Hogrefe <hogrefejason@gmail.com>
2015-01-29 16:13:37 -0600

    Began adding variables and conditions. WIP.

---
+++ rock-paper-scissors.rb
@@ -0,0 +1,9 @@
+# Simple rock, paper, scissors game without classes
+puts "Play rock, paper, scissors. Enter r, p or s now: "
+player1_move = gets.chomp.downcase.to_s
+player2_move = ["r", "p", "s"].sample
+
+
+if player1_move = "r" || player1_move = "p" || player1_move = "s"
+  
+  
