e5ab6f96b57cc697e3f3ce8b18f0f9b097334fd1
Jason Hogrefe <hogrefejason@gmail.com>
2015-01-30 11:35:35 -0600

    Began working on #1 for Level 0.

--- rock-paper-scissors.rb
+++ rock-paper-scissors.rb
@@ -4,6 +4,9 @@ player1_move = gets.chomp.downcase.to_s
 player2_move = ["r", "p", "s"].sample
 
 
-if player1_move = "r" || player1_move = "p" || player1_move = "s"
+if player1_move != "r" || player1_move != "p" || player1_move != "s"
+  puts "You did not enter r, p or s!"
+elsif
+end
   
   
